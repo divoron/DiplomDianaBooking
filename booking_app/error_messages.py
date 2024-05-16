ce25a1de242bfd8e9aecfe445c7bfbeccd8abebc
Diana Chul <voronkadi@gmail.com>
2024-05-16 21:21:25 +0300

    Add success and error messages

---
+++ booking_app/error_messages.py
@@ -0,0 +1,1 @@
+BOOKING_OCCUPIED_ERROR = "This room is occupied on this date"
---
+++ booking_app/success_messages.py
@@ -0,0 +1,3 @@
+BOOKING_CREATED_MESSAGE = "New booking was created successfully."
+BOOKING_UPDATED_MESSAGE = "The booking was updated successfully."
+BOOKING_DELETED_MESSAGE = "The booking was deleted successfully"
