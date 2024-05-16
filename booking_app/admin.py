e4b51c8f0201112ee39a7d04aa14c06c5c7561a9
Diana Chul <voronkadi@gmail.com>
2024-05-16 21:33:01 +0300

    Register hotel_model

--- booking_app/admin.py
+++ booking_app/admin.py
@@ -1,2 +1,13 @@
 from django.contrib import admin
 
+from booking_app.models.hotel_model import Hotel
+
+
+@admin.register(Hotel)
+class HotelAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location', 'rating',)
+    list_filter = ('location', 'rating',)
+    search_fields = ('name', 'description',)
+# Класс HotelAdmin определяет пользовательские настройки отображения и взаимодействия с объектами модели Hotel
+# в административной панели. list_display указывает, какие поля будут отображаться в списке объектов, list_filter
+# добавляет возможность фильтрации по указанным полям, а search_fields позволяет выполнять поиск по указанным полям.
