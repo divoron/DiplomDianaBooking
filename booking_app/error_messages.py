e5e88ad5f325ae8bd0f430cc9e47e763ef6070d9
Diana Chul <voronkadi@gmail.com>
2024-05-16 21:34:42 +0300

    Add success and error messages

---
+++ booking_app/error_messages.py
@@ -0,0 +1,2 @@
+HOTEL_NAME_LEN_ERROR = "The length of the hotel name should be between 3 and 25 characters."
+NON_UNIQUE_HOTEL_NAME_ERROR = "This hotel name is already exists. Please write city in '()' to hotel name"
---
+++ booking_app/success_messages.py
@@ -0,0 +1,3 @@
+HOTEL_CREATED_MESSAGE = "New hotel was created successfully."
+HOTEL_UPDATED_MESSAGE = "The hotel was updated successfully."
+HOTEL_DELETED_MESSAGE = "The hotel was deleted successfully."
