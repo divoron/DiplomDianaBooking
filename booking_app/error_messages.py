b1def4fe2a9a57cb6930a2ef25577d31f426f06c
Diana Chul <voronkadi@gmail.com>
2024-05-17 16:31:39 +0300

    Alter success and error messages

--- booking_app/error_messages.py
+++ booking_app/error_messages.py
@@ -1,6 +1,9 @@
 HOTEL_NAME_LEN_ERROR = "The length of the hotel name should be between 3 and 25 characters."
 NON_UNIQUE_HOTEL_NAME_ERROR = "This hotel name is already exists. Please write city in '()' to hotel name"
 
+ROOM_TYPE_REQUIRED_ERROR = "The type for room is required."
+ROOM_TYPE_LENGTH_ERROR = "The type should contain no more than 8 characters"
+
 USERNAME_NON_UNIQUE_ERROR = "This username already exists. Try something else."
 USER_EMAIL_NON_UNIQUE_ERROR = "User with this email already exists."
 USERNAME_LEN_ERROR = "The length of the username should be between 2 and 25 characters."
--- booking_app/success_messages.py
+++ booking_app/success_messages.py
@@ -2,6 +2,10 @@ HOTEL_CREATED_MESSAGE = "New hotel was created successfully."
 HOTEL_UPDATED_MESSAGE = "The hotel was updated successfully."
 HOTEL_DELETED_MESSAGE = "The hotel was deleted successfully."
 
+ROOM_CREATED_MESSAGE = "New room was created successfully"
+ROOM_UPDATED_MESSAGE = "The room was updated successfully."
+ROOM_DELETED_MESSAGE = "The room was deleted successfully"
+
 USER_CREATED_MESSAGE = "New User was created successfully."
 USER_UPDATED_MESSAGE = "The User was updated successfully."
 USER_DELETED_MESSAGE = "The User was deleted successfully"
